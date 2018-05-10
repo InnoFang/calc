0f73afa3fc719fde14eb38ad8f0b3eb665258b2b
InnoFang <innofang@outlook.com>
2018-05-10 23:33:52 +0800

    :sparkles: define calc token

--- src/main/kotlin/Token.kt
+++ src/main/kotlin/Token.kt
@@ -1,3 +1,21 @@
 /**
  * Created by Inno Fang on 2018/5/10.
  */
+
+
+data class Token(private val type: String, private val value: Any) {
+    companion object {
+        val INTEGER = "INTEGER"
+        val PLUS = "PLUS"
+        val MINUS = "MINUS"
+        val MUL = "MUL"
+        val DIV = "DIV"
+        val LPAREN = "("
+        val RPAERN = ")"
+        val EOF = "EOF"
+    }
+
+    override fun toString(): String {
+        return "Token($type, $value)"
+    }
+}
